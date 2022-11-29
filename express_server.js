cf90c26ba464c53ee8db18291ed76cff22c37d90
Likai Liu <likailiu2@gmail.com>
2022-11-30 01:17:10 +0800

    Add a second route and and template where an instance of URL shortening is showed

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars); // don't include /views/... or the file extension ".ejs"
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
