17427b8d7294dddfc5450a563410e1a5d369682e
Likai Liu <likailiu2@gmail.com>
2022-11-30 13:40:20 +0800

    Implement a function that generates a random string consisting of 7 alpha-numeric characters for later use

--- express_server.js
+++ express_server.js
@@ -3,6 +3,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 const morgan = require('morgan');
 
+const generateRandomString = () => {
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  let randomString = "";
+  for (let i = 0; i <= 6; i++) {
+    randomString += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return randomString;
+};
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -16,6 +26,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/", (req, res) => {
